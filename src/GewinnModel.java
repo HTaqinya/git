f240c1af9e2f6e008c4c6f79a1bae395a421b547
Hamid <htaqinya@student.tgm.ac.at>
2026-09-25 16:24:21 +0200

    implemented the basics of the game

--- src/GewinnModel.java
+++ src/GewinnModel.java
@@ -1,2 +1,44 @@
+
+
 public class GewinnModel {
+    private int gesamtPunkte;
+    private int spielerZahl;
+    private int computerZahl;
+    private int rundenErgebnis;
+
+    public GewinnModel() {
+        this.gesamtPunkte = 30;
+        this.spielerZahl = 0;
+        this.computerZahl = 0;
+        this.rundenErgebnis = 0;
+    }
+
+    public int getGesamtPunkte() {
+        return gesamtPunkte;
+    }
+
+    public int getComputerZahl() {
+        return computerZahl;
+    }
+
+    public int getRundenErgebnis() {
+        return rundenErgebnis;
+    }
+
+    public void berechneComputerZahl() {
+
+    }
+
+    public void berechneRunde(int spielerZahl) {
+
+    }
+
+    public boolean hatGewonnen() {
+
+
+    }
+
+    public boolean hatVerloren() {
+
+    }
 }
